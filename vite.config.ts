71e82850ba73070ef388680dd4a3792c46849bdc
Sebastian <s.kossakowski.uk@gmail.com>
2025-08-19 08:57:39 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  build: {
+    rollupOptions: {
+      external: [
+        'jspdf',
+        'jspdf-autotable'
+      ]
+    }
+  }
 })
