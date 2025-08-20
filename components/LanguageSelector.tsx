853f2c20d929df5f9c6da02d50ac5c0346510819
Sebastian <s.kossakowski.uk@gmail.com>
2025-08-20 09:10:20 +0100

    Update LanguageSelector.tsx

--- components/LanguageSelector.tsx
+++ components/LanguageSelector.tsx
@@ -1,5 +1,6 @@
 import { useState } from "preact/hooks";
 import { useTranslation } from '../i18n';
+import styles from './LanguageSelector.module.css';
 
 export const LanguageSelector = () => {
     const { language, setLanguage } = useTranslation();
@@ -14,13 +15,13 @@ export const LanguageSelector = () => {
     const selectedLanguage = languages.find(lang => lang.code === language);
 
     return (
-        <div class="language-selector" onBlur={() => setIsOpen(false)} tabIndex={0}>
-            <button class="selector-button" onClick={() => setIsOpen(!isOpen)}>
+        <div class={styles.languageSelector} onBlur={() => setIsOpen(false)} tabIndex={0}>
+            <button class={styles.selectorButton} onClick={() => setIsOpen(!isOpen)}>
                 {selectedLanguage?.flag}
-                <span class="arrow">{isOpen ? '▲' : '▼'}</span>
+                <span class={styles.arrow}>{isOpen ? '▲' : '▼'}</span>
             </button>
             {isOpen && (
-                <ul class="selector-dropdown">
+                <ul class={styles.selectorDropdown}>
                     {languages.map(lang => (
                         <li key={lang.code} onMouseDown={() => { setLanguage(lang.code); setIsOpen(false); }}>
                             {lang.flag} {lang.name}
