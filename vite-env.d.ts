6eb44d3e45d7e9644170c649adbc8e7c80a0d70f
Sebastian <s.kossakowski.uk@gmail.com>
2025-08-22 21:46:46 +0100

    Update vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -2,3 +2,14 @@ declare module '*.module.css' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+interface FileSystemFileHandle {
+  getFile(): Promise<File>;
+  createWritable(): Promise<FileSystemWritableFileStream>;
+  readonly kind: 'file';
+  readonly name: string;
+}
+
+interface Window {
+  showOpenFilePicker(options?: any): Promise<FileSystemFileHandle[]>;
+}
